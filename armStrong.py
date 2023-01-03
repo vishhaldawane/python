d679a81a9ff28899325654bfef912e2c53bb0af8
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-01-03 12:21:48 +0530

    Add files via upload

---
+++ armStrong.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+num=int(input('enter number : '))
+
+sum=0
+
+temp = num
+while temp > 0:
+  digit = temp %10
+  sum += digit ** 3
+  temp //= 10
+
+if num == sum:
+  print(num," is an armstrong")
+else:
+  print(num,"is not armstrong")
+
